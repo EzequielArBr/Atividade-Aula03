eea8ba401dfb7ed59ef338d48272b7ab2d9b39de
Ezequiel Perez <ezzeqquiel@hotmail.com>
2020-10-20 23:44:29 -0300

    Projeto Primeira versão

---
+++ src/app/livros/livro-inserir/livro-inserir.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Livro } from '../livro.model';
+
+@Component({
+  selector: 'app-livro-inserir',
+  templateUrl: './livro-inserir.component.html',
+  styleUrls: ['./livro-inserir.component.css']
+})
+export class LivroInserirComponent {
+
+  @Output()
+  livroAdicionado = new EventEmitter<Livro>();
+
+  id: string;
+  titulo: string;
+  autor: string;
+  numeroPaginas: number;
+
+
+  onInserirLivro(){
+
+    const livro: Livro = {
+
+      id: this.id,
+      titulo: this.titulo,
+      autor: this.autor,
+      numeroPaginas: this.numeroPaginas,
+
+    };
+    this.livroAdicionado.emit(livro);
+
+  }
+
+}
